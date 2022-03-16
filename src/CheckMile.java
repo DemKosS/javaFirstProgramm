9eed86ba3846c2d4b6242d92f3e3f3fce8ef6b4e
Konstantin Sid <96749252+DemKosS@users.noreply.github.com>
2022-03-16 15:42:01 +0300

    Update CheckMile.java

--- src/CheckMile.java
+++ src/CheckMile.java
@@ -2,7 +2,7 @@ public class CheckMile {
     public static void main(String[] args) {
         int priceTicket = 100;
         int priceForMile = 20;
-        int totalMileForTicket = priceTicket/priceForMile;
+        int totalMileForTicket = priceTicket / priceForMile;
         System.out.println(totalMileForTicket);
 
     }
