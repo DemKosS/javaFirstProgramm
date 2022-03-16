320a55a89483bac7ad697ca645b38c7264af531e
Сидякин Константин Николаевич <SidiakinKN@dp.mosreg.ru>
2022-03-16 14:52:21 +0300

    initial

---
+++ src/CheckMile.java
@@ -0,0 +1,9 @@
+public class CheckMile {
+    public static void main(String[] args) {
+        int priceTicket = 100;
+        int priceForMile = 20;
+        int totalMileForTicket = priceTicket/priceForMile;
+        System.out.println(totalMileForTicket);
+
+    }
+}
